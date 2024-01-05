4f8326f023968a4c0a325ce336d5ab84d14789d8
olyshaaa <olgasevel108@gmail.com>
2024-01-05 16:18:46 +0200

    vercel cors

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -2,7 +2,7 @@
 import { Navigate, Outlet } from 'react-router-dom'
 
 const Protected = () => {
-    const token = localStorage.getItem('authenticated')
+  const token = localStorage.getItem('authenticated')
   return (
     token ? <Outlet /> : <Navigate to="/login" />
   )
